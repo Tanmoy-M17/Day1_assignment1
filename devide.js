8fb1cf570cfdf69276bfe7b02947890630f73a6b
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-03-02 23:18:35 +0530

    Done
    Day1 Assignment

---
+++ devide.js
@@ -0,0 +1,5 @@
+function devide(a,b){
+    return a/b;
+    
+}
+module.exports =devide;
---
+++ index.js
@@ -0,0 +1,14 @@
+
+let a=15;
+let b=6;
+
+
+const add = require("./add");
+const subtract = require("./subtract");
+const multiply = require("./multiply");
+const devide = require("./devide");
+
+console.log(add(a,b))
+console.log(subtract(a,b))
+console.log(multiply(a,b))
+console.log(devide(a,b))
